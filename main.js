53dd271c2d602bd37676f3e96d5e754223136dc2
HootyMeowyCossettey <91523274+HootyMeowyCossettey@users.noreply.github.com>
2021-10-22 20:23:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+menu_list_array = ["Veg Margherita Pizza","Chicken Extravaganza","Cheesy Delight", "Panner Pizza"];
+customer_array=[]
+function showMenu(){
+    document.getElementById("menu").style.display="inline-block";
+}
+function makePizza(){
+    document.getElementById("customers-pixxa").style.display="inline-block";  
+    document.getElementById(piva).push(document.getElementById("text-impit").value);
+    document.getElementById(piva).push(document.getElementById("text-imptit").value);
+
+} 
